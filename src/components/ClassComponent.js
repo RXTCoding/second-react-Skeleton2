2029cb60b88ed233ce532e9d749e6eed6c2f9d58
RXTCoding <60244212+RXTCoding@users.noreply.github.com>
2021-04-21 16:38:00 -0700

    wlr1 unit3 day3 app1

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -3,12 +3,27 @@ import React, { Component } from "react";
 class ClassComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      inputValue: ""
+    };
   }
+
+  handleChange = (value) => {
+    this.setState({
+      inputValue: value
+    });
+  };
+
   render() {
     return (
       <div>
         <h3> Hey from Class Component</h3>
+        <input
+          value={this.state.inputValue}
+          onChange={(e) => this.handleChange(e.target.value)}
+          type="text"
+        />
+        <p>{this.state.inputValue}</p>
       </div>
     );
   }
